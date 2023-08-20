1a20d278cfce400185d56b779a862d139ead239b
Ivymaundu <ivymaundu03@gmail.com>
2023-08-20 18:31:05 +0300

    task

---
+++ mystrings.py
@@ -0,0 +1,18 @@
+first_name = 'Ivy'
+last_name = 'Maundu'
+full_name = first_name + last_name
+# accessing methods using dots within a string
+full_name = full_name.lower()
+print(full_name.swapcase())
+print(full_name.isdigit())
+
+num1=int('7')
+num2=int('8')
+t= num1+num2
+print(t)
+# indexing and slicing
+# slicing-used to access multiple characters
+# indexing-used to acces one character
+
+print(full_name[-1])
+
---
+++ stringtask.py
@@ -0,0 +1,27 @@
+# Clean up the following variable to give the clean version in lower case. Using inbuilt methods in the str class
+# name = “  JOHn  .“ to “john”
+name = ' JoHn  .'
+name = name.strip().lower()
+print(name[0:4])
+
+# Slice the below string to get you the resulting sentence:
+# sentence_one = “The Dog Breed is German Shepherd” only display “Breed is German”
+# sentence_two = “Defeats for the Clinton forces, this was her moment of triumph” only display “Clinton forces”
+sentence_one = "The Dog Breed is German Shepherd"
+print(sentence_one[8:23])
+sentence_two = "Defeats for the Clinton forces, this was her moment of triumph"
+print(sentence_two[16:30])
+
+# Split the below sentence using a semicolon i.e ; And display length of the result. 
+# “The lazy dog; ran so fast; it hit the wall.” 
+sentence = "The lazy dog; ran so fast; it hit the wall."
+sentence = sentence.split(";")
+print(sentence)
+
+# first_name="  Joh.n"  last_name="   Do,e" Clean up and display Full name i.e John Doe
+first_name = "  Joh.n"
+last_name="   Do,e"
+first_name=first_name.strip().replace('.' , '')
+last_name=last_name.strip().replace(',','').replace('D',' D')
+full_name=first_name+last_name
+print(full_name)
